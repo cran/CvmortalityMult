bf88ee9aeaff65abdc5db616b0f533ffdcee4c11
David Atance <david.atance@uah.es>
2024-05-06 16:30:03 +0000

    version 1.0.1

--- R/SpainNat.R
+++ R/SpainNat.R
@@ -6,7 +6,7 @@
 #'
 #' @name SpainNat
 #'
-#' @format A data frame with 600 rows and 9 columns with the following information
+#' @format A data frame with 600 rows and 9 columns with class \code{"CVmortalityData"} including the following information
 #' * `ccaa` a vector containing all the regions of Spain. Indeed, the column takes the following information: Spain.
 #' * `years` a vector containing the periods of the dataset from 1991 to 2020.
 #' * `ages` a vector containing the abridged ages considered in the dataset, 0, <1, 1-4, 5-9, 10-14, 15-19, 20-24, 25-29, 30-34, 35-39, 40-44, 45-49, 50-54, 55-59, 60-64, 65-69, 70-74, 75-79, 80-84, 85-89, and 90-94.
@@ -35,9 +35,11 @@
 #' #An example to how the additive multi-population model fits the data
 #' ages <- c(0, 1, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90)
 #' library(gnm)
-#' LC_Spainmales <- fit_additive.LC.multi(qxt = SpainNat$qx_male,
-#'                                        periods = c(1991:2020),
-#'                                        ages = ages,
-#'                                        nPop = 1)
+#' LC_Spainmales <- fitLCmulti(qxt = SpainNat$qx_male,
+#'                             periods = c(1991:2020),
+#'                             ages = ages,
+#'                             nPop = 1)
+#'
+#' LC_Spainmales
 #' }
 "SpainNat"
